341201582c5e6c181e2d646204a7e53c338bf5bf
ddjerqq <57017344+ddjerqq@users.noreply.github.com>
2024-02-14 23:18:00 +0000

    Deploying to gh-pages from @ ddjerqq/klean@01b3f7bb52e2a4c38f50ae8e7c48423b6fa72bfe 🚀

---
+++ Layout/ThemeToggle.razor.js
@@ -0,0 +1,7 @@
+export const setTheme = (theme) => {
+  if (theme === 'dark') {
+    document.documentElement.classList.add('dark');
+  } else if (theme === 'light') {
+    document.documentElement.classList.remove('dark');
+  }
+};
