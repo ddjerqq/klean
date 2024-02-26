6cd294d8b5fffb5aeffc441046443c33e798a469
ddjerqq <ddjerqq@gmail.com>
2024-02-27 03:09:56 +1300

    start adding back rate limiting but now it will be more managed

--- src/Infrastructure/ConfigureInfrastructure.cs
+++ src/Infrastructure/ConfigureInfrastructure.cs
@@ -1,9 +1,15 @@
 using System.ComponentModel;
+using System.Security.Claims;
+using System.Threading.RateLimiting;
 using Application.Abstractions;
 using Infrastructure;
 using Infrastructure.Idempotency;
+using Infrastructure.RateLimiting;
 using Infrastructure.Services;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 [assembly: HostingStartup(typeof(ConfigureInfrastructure))]
@@ -23,5 +29,52 @@ public class ConfigureInfrastructure : IHostingStartup
             services.AddIdempotency();
             services.AddMemoryCache();
         });
+
+        // add rate limiting
+        builder.ConfigureServices(services =>
+        {
+
+        });
+    }
+
+    public static IServiceCollection AddRateLimiting(IServiceCollection services, IConfiguration configuration)
+    {
+        var policies = RateLimitConstants.LoadRateLimitOptions(configuration)
+            .ToList();
+
+        var globalPolicy = policies
+            .First(x => x.PolicyName == RateLimitConstants.GlobalPolicyName);
+
+        services.AddRateLimiter(rateLimitOptions =>
+        {
+            rateLimitOptions.RejectionStatusCode = 429;
+
+            rateLimitOptions.OnRejected = (ctx, _) =>
+            {
+                if (ctx.Lease.TryGetMetadata(MetadataName.RetryAfter, out var retryAfter))
+                    ctx.HttpContext.Response.Headers.RetryAfter = retryAfter.ToString("R");
+
+                return ValueTask.CompletedTask;
+            };
+
+            rateLimitOptions.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(context =>
+            {
+                var key = context.Connection.RemoteIpAddress?.ToString() ?? context.Connection.Id;
+
+                if (context.User is { Identity.IsAuthenticated: true, Claims: var claims })
+                {
+                    var id = claims
+                        .FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?
+                        .Value;
+
+                    if (!string.IsNullOrEmpty(id))
+                        key = id;
+                }
+
+                return RateLimitPartition.GetTokenBucketLimiter(key, _ => globalPolicy);
+            });
+        });
+
+        return services;
     }
 }
