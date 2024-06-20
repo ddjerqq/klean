ac2e57c4e92dabc4487714d5a4f8f7759ccb8d13
10x developer <gio2004n@gmail.com>
2024-06-20 21:01:09 +0400

    start adding source generators
    Signed-off-by: 10x developer <gio2004n@gmail.com>

---
+++ src/source_generators/StrongIdGenerator.Sample/Program.cs
@@ -0,0 +1,7 @@
+﻿using Klean.Generated;
+
+var person = new Person(PersonId.NewPersonId());
+Console.WriteLine("hello im mr frog, hello");
+
+[StrongId(typeof(Ulid))]
+public record Person(PersonId Id);
---
+++ src/source_generators/StrongIdGenerator/Contexts.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+
+namespace StrongIdGenerator;
+
+internal readonly record struct EntityStrongIdContext(string? Namespace, string TypeName, string IdType)
+{
+    public static EntityStrongIdContext FromEntityTypeInfo((INamedTypeSymbol EntityType, INamedTypeSymbol IdType)? typeInfo, CancellationToken ct)
+    {
+        var type = typeInfo!.Value;
+        var ns = type.EntityType.ContainingNamespace.IsGlobalNamespace
+            ? null
+            : type.EntityType.ContainingNamespace.ToDisplayString();
+
+        var name = type.EntityType.Name;
+        var targetType = type.IdType.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat);
+
+        return new EntityStrongIdContext(ns, name, targetType);
+    }
+}
+
+internal sealed class PartialClassContextEqualityComparer : EqualityComparer<EntityStrongIdContext>
+{
+    private PartialClassContextEqualityComparer() {}
+    public static IEqualityComparer<EntityStrongIdContext> Instance { get; } = new PartialClassContextEqualityComparer();
+
+    public override bool Equals(EntityStrongIdContext x, EntityStrongIdContext y) =>
+        x.Namespace == y.Namespace
+        && x.TypeName == y.TypeName
+        && x.IdType == y.IdType;
+
+    public override int GetHashCode(EntityStrongIdContext obj) => HashCode.Combine(obj.Namespace, obj.TypeName, obj.IdType);
+}
