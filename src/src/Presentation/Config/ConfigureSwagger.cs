5067c8d7229ae29f9ff9d966658efbefdb4e6cd9
ddjerqq <57017344+ddjerqq@users.noreply.github.com>
2024-11-12 21:47:04 +0400

    configure source generated mapping for swagger
    Signed-off-by: ddjerqq <57017344+ddjerqq@users.noreply.github.com>

--- src/src/Presentation/Config/ConfigureSwagger.cs
+++ src/src/Presentation/Config/ConfigureSwagger.cs
@@ -1,6 +1,8 @@
 #pragma warning disable CS1591
 using System.Reflection;
 using Application;
+using Domain.Aggregates;
+using Generated;
 using Microsoft.OpenApi.Models;
 
 namespace Presentation.Config;
@@ -15,16 +17,22 @@ public sealed class ConfigureSwagger : ConfigurationBase
         services.AddEndpointsApiExplorer();
         services.AddSwaggerGen(options =>
         {
-            var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-            var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-            options.IncludeXmlComments(xmlPath);
-            options.SupportNonNullableReferenceTypes();
+            var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
 
+            options.MapType<Ulid>(() => new OpenApiSchema { Type = "string" });
+
+            // configure source generated type conversions
+            var idTypes = Domain.Domain.Assembly.GetTypes().Where(t => t.GetInterfaces().Any(i => i == typeof(IStrongId)));
+            foreach (var type in idTypes)
+                options.MapType(type, () => new OpenApiSchema { Type = "string" });
+
+            options.SupportNonNullableReferenceTypes();
             options.SwaggerDoc("v1", new OpenApiInfo
             {
                 Title = "klean",
                 Version = "v1",
-                Description = "klean API",
+                Description = "klean architecture API",
             });
 
             options.AddSecurityRequirement(new OpenApiSecurityRequirement
