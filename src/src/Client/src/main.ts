f17fd914dd1a9d9480b3447a56db5ce731a68b6c
ddjerqq <57017344+ddjerqq@users.noreply.github.com>
2024-11-24 04:04:40 +0400

    removed svelte
    Signed-off-by: ddjerqq <57017344+ddjerqq@users.noreply.github.com>

--- src/src/Client/src/main.ts
+++
@@ -1,4 +0,0 @@
-﻿import { mount } from "svelte";
-import App from "./App.svelte";
-
-export const app = mount(App, { target: document.getElementById("app") as HTMLElement });
--- src/src/Client/src/routes/cards/DemoContainer.svelte
+++
@@ -1,9 +0,0 @@
-﻿<script lang="ts">
-  import type { Snippet } from 'svelte';
-
-  let { children }: { children: Snippet } = $props();
-</script>
-
-<div class="flex items-center justify-center [&>div]:w-full">
-  {@render children?.()}
-</div>
