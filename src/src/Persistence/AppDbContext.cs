6d0bf4ef5c92153954fbe01da2e46c485c2e4827
ddjerqq <57017344+ddjerqq@users.noreply.github.com>
2024-11-26 16:18:28 +0400

    fixed return url on login
    Signed-off-by: ddjerqq <57017344+ddjerqq@users.noreply.github.com>

--- src/src/Persistence/AppDbContext.cs
+++ src/src/Persistence/AppDbContext.cs
@@ -45,7 +45,6 @@ public sealed class AppDbContext(
         base.ConfigureConventions(builder);
     }
 
-    // TODO move to ext
     private static void SnakeCaseRename(ModelBuilder builder)
     {
         foreach (var entity in builder.Model.GetEntityTypes())
